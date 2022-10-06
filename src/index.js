0dbb5cf781523d411b78088074637f4724c840a0
Rosa Regalado <rosaregalado101@gmail.com>
2022-10-05 22:22:27 -0700

    re-install lib gives npmengine error

--- src/index.js
+++ src/index.js
@@ -1,3 +1,3 @@
-const library = require('@rosaregalado/phone-number-libraryy')
+const library = require('@rosaregalado/phone-number-library@1.0.1')
 const number = library.phoneFormat('5101234567')
 console.log(number)
